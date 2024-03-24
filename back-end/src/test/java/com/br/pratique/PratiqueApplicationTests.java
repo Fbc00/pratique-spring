8a36a9e7ffe9412936395cbac0b84374a1123284
fabricioassuncao <fabricio.assuncao@buser.com.br>
2024-03-24 01:14:40 -0300

    front

---
+++ back-end/src/test/java/com/br/pratique/PratiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pratique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
